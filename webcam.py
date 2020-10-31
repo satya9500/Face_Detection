918d8bca2e75cb7716c605cd4137ffd6b51d9279
Satya-here <satya.prakash9500@gmail.com>
2020-10-31 16:23:45 +0530

    py file added

---
+++ webcam.py
@@ -0,0 +1,19 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+classifier = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+while True:
+    ret, frame = cap.read()
+    if ret:
+       faces = classifier.detectMultiScale(frame)
+       for face in faces:
+            x,y,w,h=face
+            frame = cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 3)
+       cv2.imshow("My Window", frame) 
+    key = cv2.waitKey(30)
+    if(key == ord("q")):
+        break
+        
+cap.release()
+cv2.destroyAllWindows()
